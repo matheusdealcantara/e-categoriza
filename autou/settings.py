998acf14264baa1911fc3af655682ab8e6c5a1de
matheusdealcantara <dealcantaramatheus1@gmail.com>
2025-09-15 23:43:56 -0300

    feat: adiciona estrutura para o postgresql

--- autou/settings.py
+++ autou/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -83,12 +84,22 @@ WSGI_APPLICATION = 'autou.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / os.getenv('DB_NAME', 'db.sqlite3'),
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv('DB_NAME', 'local_db'),
+            "USER": os.getenv('DB_USER', 'local_user'),
+            "PASSWORD": os.getenv('DB_PASSWORD', 'local_password'),
+            "HOST": os.getenv('DB_HOST', 'localhost'),
+            "PORT": os.getenv('DB_PORT', '5432'),
+        }
+    }
 
 
 # Password validation
