92c071bc17af793db5cc3c8adb7d8ce5e33b88dd
matheusdealcantara <dealcantaramatheus1@gmail.com>
2025-09-15 23:17:08 -0300

    feat: altera lógica de inferência

--- api/utils/categorize.py
+++ api/utils/categorize.py
@@ -1,16 +1,17 @@
 # Essa função é responsável por classificar os emails entre produtiovo e,
-# improdutivo. Ela utiliza um modelo de classificação de texto pré-treinado
+# improdutivo.
 import os
 
 from dotenv import load_dotenv
-from huggingface_hub import InferenceClient
+from openai import OpenAI
 
 # Load environment variables from .env file
 load_dotenv()
-client = InferenceClient(api_key=os.getenv('HF_API_KEY'))
+client = OpenAI(api_key=os.getenv('NSCALE_SERVICE_TOKEN'), 
+                base_url="https://inference.api.nscale.com/v1")
 
 def classify_text(text: str):
-    message = {
+    message = [{
         "role": "user",
         "content": f"""
         Você é um assistente virtual que deve categorizar emails entre "Produtivo" ou "Improdutivo", onde emails produtivos são 
@@ -20,20 +21,12 @@ def classify_text(text: str):
         informações desnecessárias, etc). 
         Responda apenas com a palavra "Produtivo" ou "Improdutivo".
         Classifique o email abaixo:\n\nEmail:\n{text}"""
-    }
+    }]
 
-    stream = client.chat.completions.create(
-        model="meta-llama/Llama-3.2-3B-Instruct", 
-        messages=[message], 
-        temperature=0.5,
-        max_tokens=2048,
-        top_p=0.7,
-        stream=True
+    response = client.chat.completions.create(
+        model="Qwen/Qwen3-4B-Instruct-2507", 
+        messages= message,
     )
 
 
-    response_text = ""
-    for chunk in stream:
-        response_text += chunk.choices[0].delta.content
-
-    return response_text.strip()
+    return response.choices[0].message.content
