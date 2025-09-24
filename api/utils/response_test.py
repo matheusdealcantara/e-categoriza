c92b290469ae4993d2f7aacee06ef9fa87e5404f
Matheus de Alcântara <107211702+matheusdealcantara@users.noreply.github.com>
2025-09-24 03:41:16 +0000

    chore: delete out of scope utils

--- api/utils/response_test.py
+++
@@ -1,7 +0,0 @@
-from generate_response import generate_response
-
-email_text = """
-Olá, gostaria de uma atualização sobre o projeto.
-"""
-
-print("Resposta gerada: ", generate_response(email_text))
